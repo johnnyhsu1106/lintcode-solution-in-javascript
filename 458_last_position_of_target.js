c3d9bbdc3727448391819efe2e59de8960f68608
Johnny Hsu <hsuyuwei@Johnnys-Macbook-Pro.local>
2017-10-05 23:08:13 -0400

    upload solution

--- 458_last_position_of_target.js
+++ 458_last_position_of_target.js
@@ -1,3 +1,34 @@
-const lastPostion = () => {
+'use strict';
+const lastPostion = (nums, target) => {
 
+    let start = 0, end = nums.length - 1;
+    while (start + 1 < end) {
+        let mid = Math.floor(start + (end - start) / 2);
+
+        if (nums[mid] > target) {
+            end = mid;
+        } else if (nums[mid] < target) {
+            start = mid;
+        } else {
+            start = mid;
+        }
+    }
+    if (nums[end] === target) {
+        return end;
+    }
+    if (nums[start] === target) {
+        return start;
+    }
+    return -1;
 };
+
+
+
+// const main = () => {
+//     const nums = [1, 2, 2, 4, 5, 5];
+//     console.log(lastPostion(nums, 2) === 2);
+//     console.log(lastPostion(nums, 5) === 5);
+//     console.log(lastPostion(nums, 6) === -1)
+//
+// }
+// main();
---
+++ 459_closest_number_in_sorted_array.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const closestNumber =  (nums, target) => {
+    let start = 0, end = nums.length - 1;
+    while (start + 1 < end) {
+        let mid = Math.floor(start + (end - start) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }else if (nums[mid] < target) {
+            start = mid;
+        }else {
+            end = mid;
+        }
+    }
+    if (target - nums[start] > nums[end] - target) {
+        return end;
+    }
+    return start;
+};
+
+
+const main = () => {
+    let nums;
+    nums = [1, 2, 3];
+    console.log(closestNumber(nums, 2) === 1);
+
+    nums = [1, 4, 6];
+    console.log(closestNumber(nums, 3) === 1);
+
+    nums = [1, 4, 6];
+    console.log(closestNumber(nums, 5) === 1 || closestNumber(nums, 5) === 2);
+
+    nums = [1, 3, 3, 4];
+    console.log(closestNumber(nums, 2) === 0 || closestNumber(nums, 2) === 1 || closestNumber(nums, 2) === 2);
+};
+main();
